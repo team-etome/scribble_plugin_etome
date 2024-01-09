fc341a502cbca6f1936e5fb2ba86ee4c8df8b61e
leywino <muhammadarshadv2@gmail.com>
2024-01-10 03:16:45 +0530

    removed warnings and formatted code

--- android/src/main/kotlin/com/leywin/scribble_etome/ScribbleEtomeView.kt
+++ android/src/main/kotlin/com/leywin/scribble_etome/ScribbleEtomeView.kt
@@ -1,4 +1,6 @@
 package com.leywin.scribble_etome
+
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -10,7 +12,6 @@ import android.util.Log
 import android.view.HandwrittenView2
 import android.view.View
 import android.widget.Space
-import android.widget.TextView
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.platform.PlatformView
@@ -24,13 +25,13 @@ import java.util.Locale
 import kotlin.system.exitProcess
 
 
-class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, channel: MethodChannel) : PlatformView  {
+class HandwrittenView(
+    context: Context, creationParams: Map<String?, Any?>?, channel: MethodChannel
+) : PlatformView {
     private var buttonLock = false
     private var initFlag = false
     var mHandwrittenView: HandwrittenView2? = null
-    private var strokeTv: TextView? = null
-    private val mHandler = InitHandler(creationParams,context)
-    private var strokeType = 0
+    private val mHandler = InitHandler(creationParams)
     private var layout: View = View.inflate(context, R.layout.activity_main, null)
 
     override fun getView(): View {
@@ -40,6 +41,7 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
     override fun dispose() {
         mHandwrittenView?.destoryView()
     }
+
     init {
         mHandwrittenView = layout.findViewById(R.id.handwrittenView)
         val topPaddingHeight = creationParams!!["topPaddingHeight"] as Int
@@ -50,20 +52,20 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
         }
 
         channel.setMethodCallHandler { call, result ->
-            onMethodCall(call, result,context)
+            onMethodCall(call, result)
         }
 
         mHandler.sendEmptyMessageDelayed(DELAY_REFRESH, DELAY_TIME.toLong())
     }
 
-    fun setPadToppingHeight(padToppingHeight: Int) {
+    private fun setPadToppingHeight(padToppingHeight: Int) {
         val spaceView = layout.findViewById<Space>(R.id.padTopping)
         val layoutParams = spaceView.layoutParams
         layoutParams.height = padToppingHeight
         spaceView.layoutParams = layoutParams
     }
 
-    private fun onMethodCall(call: MethodCall, result:  MethodChannel.Result, context: Context) {
+    private fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         when (call.method) {
             "undo" -> undo()
             "redo" -> redo()
@@ -72,11 +74,13 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
                 val strokeType = call.argument<Int>("strokeType")
                 setPenStroke(strokeType ?: 0)
             }
+
             "save" -> {
                 val imageName = call.argument<String>("imageName")
 
                 save(result, imageName!!)
             }
+
             "destroy" -> onDestroy()
 //            "load" -> {
 //                val bitArr = call.argument<ByteArray>("bitMap")
@@ -85,21 +89,6 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
 
         }
     }
-
-    private fun load(byteArray: ByteArray, context: Context, result: MethodChannel.Result) {
-        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-
-        if (bitmap != null) {
-            mHandwrittenView!!.bitmap = bitmap
-            mHandwrittenView!!.refreshBitmap()
-            result.success(null)
-        } else {
-            val errorMessage = "Failed to convert byte array to bitmap"
-            Log.e("HandwrittenView", errorMessage)
-            result.error("BITMAP_ERROR", errorMessage, null)
-        }
-    }
-
 
 
     private fun undo() {
@@ -139,10 +128,10 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
             val byteArrayOutputStream = ByteArrayOutputStream()
             bitmap.compress(Bitmap.CompressFormat.PNG, 90, byteArrayOutputStream)
             val byteArray = byteArrayOutputStream.toByteArray()
-            val (isSaved, errorMessage) = saveBitmap(bitmap, imageName);
-            if(isSaved){
+            val (isSaved, errorMessage) = saveBitmap(bitmap, imageName)
+            if (isSaved) {
                 result.success(byteArray)
-            }else{
+            } else {
                 result.error("SAVE_ERROR", errorMessage, null)
             }
             buttonLock = false
@@ -150,14 +139,12 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
     }
 
 
-
-
-
     private val isSpecialPanel: Boolean
-
         get() = false
 
-    internal inner class InitHandler(private val creationParams: Map<String?, Any?>?, private val context: Context) : Handler(Looper.getMainLooper()) {
+    @SuppressLint("HandlerLeak")
+    internal inner class InitHandler(private val creationParams: Map<String?, Any?>?) :
+        Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
             when (msg.what) {
@@ -193,8 +180,7 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
                                     mScreenH - mBottom,
                                     mScreenW - mLeft,
                                     mScreenH - mTop
-                                ), false,
-                                Rect(
+                                ), false, Rect(
                                     mScreenW - mFilterRight,
                                     mScreenH - mFilterBottom,
                                     mScreenW - mFilterLeft,
@@ -203,15 +189,12 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
                             )
                         } else {
                             mHandwrittenView!!.initNative(
-                                Rect(mLeft, mTop, mRight, mBottom), false,
+                                Rect(mLeft, mTop, mRight, mBottom),
+                                false,
                                 Rect(mFilterLeft, mFilterTop, mFilterRight, mFilterBottom)
                             )
                         }
                         initFlag = true
-                        /**
-                         * After initialization is complete, load and refresh the previously saved handwriting image,
-                         * and perform writing and drawing operations on the original handwriting.
-                         */
                         val imageName = creationParams!!["imageName"] as String
                         val bitmap = loadBitmap(imageName)
                         if (bitmap != null) {
@@ -224,15 +207,6 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
         }
     }
 
-//    fun onResume() {
-//        super.onResume()
-//        Log.e("TAG", "onResume")
-//    }
-//
-//     fun onPause() {
-//        super.onPause()
-//        Log.e("TAG", "onPause")
-//    }
 
     private fun onDestroy() {
         if (initFlag) {
@@ -240,22 +214,10 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
             mHandwrittenView!!.exit()
         }
         mHandwrittenView!!.destoryView()
-//        super.onDestroy()
     }
 
     companion object {
-        /**
-         * 1. Put the system-compiled classes.jar in ROOT_DIR/app/libs
-         * 2. Code addition search: Make classes.jar compile before android.jar (the jar package name is subject to the actual name,
-         *    note the RK path: out/target/common/obj/JAVA_LIBRARIES/framework_intermediates/classes-header.jar)
-         * 3. Rebuild Project
-         */
-        /** English description, the same as above
-         * 1. Put the compiled classes. jar in ROOT_ DIR/app/libs
-         * 2. Code addition search: 使classes.jar包编译先于android.jar (The jar package name shall be subject to the actual name.
-         *    Note the RK path: out/target/common/obj/JAVA_LIBRARIES/framework_intermediates/classes header. jar)
-         * 3.Rebuild Project
-         */
+
         private var mScreenH = 0
         private var mScreenW = 0
         private var mLeft = 0
@@ -279,7 +241,8 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
                 return Pair(false, errMsg)
             }
 
-            val fileName = imageName ?: SimpleDateFormat("yyyyMMdd-HHmmss", Locale.getDefault()).format(Date())
+            val fileName =
+                imageName ?: SimpleDateFormat("yyyyMMdd-HHmmss", Locale.getDefault()).format(Date())
             val filePath = "$HANDWRITE_SAVE_PATH${fileName}.png"
 
             return try {
@@ -293,71 +256,6 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
                 Pair(false, errMsg) // Failure, with error message
             }
         }
-
-//        fun saveBitmap(bitmap: Bitmap, imagename: String?): Boolean {
-//            var imagename = imagename
-//            val file = File(HANDWRITE_SAVE_PATH)
-//
-//            if (!file.exists()) {
-//                val flag: Boolean = file.mkdirs()
-//            }
-//
-//            if (imagename == null) {
-//                val df = SimpleDateFormat("yyyyMMdd-HHmmss")
-//                imagename = df.format(Date())
-//                Log.d("leywin", "image name is null")
-//            } else {
-//                Log.d("leywin", "image name is not null")
-//            }
-//
-//            val path: String = HANDWRITE_SAVE_PATH + imagename + ".png"
-//            var fos: FileOutputStream? = null
-//
-//            try {
-//                fos = FileOutputStream(path)
-//
-//                if (fos != null) {
-//                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, fos)
-//                    fos.close()
-//                    return true // Image successfully saved
-//                }
-//            } catch (e: Exception) {
-//                Log.d("leywin", "didnt save")
-//                Log.v("xml_log_err", "" + e.toString())
-//                e.printStackTrace()
-//            }
-//
-//            return false // Image not saved due to an exception
-//        }
-
-
-//        fun saveBitmap( bitmap: Bitmap, imageName: String?,context: Context): Pair<Boolean, String?> {
-//            // Use the app-specific external files directory for storage
-//            val directory = context.getExternalFilesDir(null)
-//            val folder = File(directory, "Etome")
-//
-//            if (!folder.exists() && !folder.mkdirs()) {
-//                val errMsg = "Failed to create directory: ${folder.absolutePath}"
-//                Log.e("HandwrittenView", errMsg)
-//                return Pair(false, errMsg)
-//            }
-//
-//            val fileName = imageName ?: SimpleDateFormat("yyyyMMdd-HHmmss", Locale.getDefault()).format(Date())
-//            val filePath = File(folder, "${fileName}.png").absolutePath
-//
-//            return try {
-//                FileOutputStream(filePath).use { fos ->
-//                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, fos)
-//                }
-//                Pair(true, null) // Success, no error message
-//            } catch (e: IOException) {
-//                val errMsg = "Error saving bitmap: ${e.localizedMessage}"
-//                Log.e("HandwrittenView", errMsg, e)
-//                Pair(false, errMsg) // Failure, with error message
-//            }
-//        }
-
-
 
 
         fun loadBitmap(imageName: String): Bitmap? {
@@ -382,11 +280,5 @@ class HandwrittenView(context: Context, creationParams: Map<String?, Any?>?, cha
                 null
             }
         }
-
-
-
-
-
-
     }
 }
